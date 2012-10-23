3aa400e9d8a389700a1b0b8542d2ff1ae3f81540
Mario Rimann <mario@rimann.org>
2012-10-23 22:45:02 +0200

    [FEATURE] Render list of translation units and their status

--- Classes/Mrimann/XliffTranslator/Controller/StandardController.php
+++ Classes/Mrimann/XliffTranslator/Controller/StandardController.php
@@ -22,6 +22,18 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	protected $packageManager;
 
 	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\I18n\Service
+	 */
+	protected $localizationService;
+
+	/**
+	 * @Flow\Inject
+	 * @var \TYPO3\Flow\I18n\Xliff\XliffParser
+	 */
+	protected $xliffParser;
+
+	/**
 	 * Index action
 	 *
 	 * @return void
@@ -39,15 +51,62 @@ class StandardController extends \TYPO3\Flow\Mvc\Controller\ActionController {
 	 * @param string $toLang
 	 */
 	public function translateAction($packageKey = '', $fromLang = '', $toLang = '') {
-		if ($fromLang != '' && $toLang != '') {
-			$this->view->assign('readyForTranslating', TRUE);
-		}
 		$this->view->assign('packageKey', $packageKey);
 		$this->view->assign('languages', explode(',', $this->settings['availableLanguages']));
 		$this->view->assign('fromLang', $fromLang);
 		$this->view->assign('toLang', $toLang);
+
+		// check if we're ready for the actual translation
+		if ($fromLang != '' && $toLang != '') {
+			$this->view->assign('readyForTranslating', TRUE);
+			$this->view->assign('translationMatrix', $this->generateTranslationMatrix($packageKey, $fromLang, $toLang));
+		}
 	}
 
+	protected function generateTranslationMatrix($packageKey, $fromLang, $toLang) {
+		$fromLocale = new \TYPO3\Flow\I18n\Locale($fromLang);
+		$fromItems = $this->getModel($packageKey, 'Main', $fromLocale);
+
+		$toLocale = new \TYPO3\Flow\I18n\Locale($toLang);
+		$toItems = $this->getModel($packageKey, 'Main', $toLocale);
+
+		foreach ($fromItems['translationUnits'] as $transUnitId => $value) {
+			$matrix[$transUnitId]['source'] = $value[0]['source'];
+
+			// check if untranslated
+			if (!isset($toItems['translationUnits'][$transUnitId][0]['target'])
+				|| $toItems['translationUnits'][$transUnitId][0]['target'] == ''
+			) {
+				$matrix[$transUnitId]['target'] = '';
+				$matrix[$transUnitId]['nonTranslated'] = TRUE;
+				$matrix[$transUnitId]['class'] = 'nonTranslated';
+			} else {
+				$matrix[$transUnitId]['target'] = $toItems['translationUnits'][$transUnitId][0]['target'];
+				// check if original text was modified
+				if ($toItems['translationUnits'][$transUnitId][0]['source'] != $value[0]['source']) {
+					$matrix[$transUnitId]['originalModified'] = TRUE;
+					$matrix[$transUnitId]['class'] = 'modified';
+					$matrix[$transUnitId]['originalSource'] = $toItems['translationUnits'][$transUnitId][0]['source'];
+				} else {
+					// translation available AND original text is still the same
+					$matrix[$transUnitId]['perfetto'] = TRUE;
+					$matrix[$transUnitId]['class'] = 'ok';
+				}
+			}
+		}
+
+		return $matrix;
+	}
+
+
+	protected function getModel($packageKey, $sourceName, \TYPO3\Flow\I18n\Locale $locale) {
+		$sourcePath = \TYPO3\Flow\Utility\Files::concatenatePaths(array('resource://' . $packageKey, 'Private/Translations'));
+		list($sourcePath, $foundLocale) = $this->localizationService->getXliffFilenameAndPath($sourcePath, $sourceName, $locale);
+
+		$result = $this->xliffParser->getParsedData($sourcePath);
+		return $result;
+		return new \TYPO3\Flow\I18n\Xliff\XliffModel($sourcePath, $foundLocale);
+	}
 }
 
 ?>
